82a491449ec04a56c1b8f0837a5e25ecbc1179d9
Enngage <ennspassion@gmail.com>
2018-01-09 14:56:25 +0100

    Updates file name & removes colors

--- lib/library-version.ts
+++ lib/library-version.ts
@@ -1,4 +1,4 @@
 
-export const version = '1.0.0';
+export const version = '1.0.1';
 export const packageId = 'kentico-cloud-delivery-node-sdk';
 export const repoHost = 'npmjs.com'
--- misc/update-sdk-info.js
+++ misc/update-sdk-info.js
@@ -1,10 +1,9 @@
 const path = require('path');
-const colors = require('colors/safe');
 const fs = require('fs');
 const appVersion = require('../package.json').version;
 const packageId = require('../package.json').name;
 
-console.log(colors.cyan('\nRunning pre-build tasks'));
+console.log('\nRunning pre-build tasks');
 
 const versionFilePath = path.join(__dirname + '/../lib/library-version.ts');
 
@@ -17,9 +16,9 @@ export const repoHost = 'npmjs.com'
 // ensure version module pulls value from package.json
 fs.writeFile(versionFilePath, src, { flat: 'w' }, function (err) {
     if (err) {
-        return console.log(colors.red(err));
+        return console.log(err);
     }
 
-    console.log(colors.green(`Updating application version ${colors.yellow(appVersion)}`));
-    console.log(`${colors.green('Writing version module to ')}${colors.yellow(versionFilePath)}\n`);
+    console.log(`Updating application version ${appVersion}`);
+    console.log(`${'Writing version module to '}${versionFilePath}\n`);
 });
